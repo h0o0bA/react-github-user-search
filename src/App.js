62b42a0617f56adf4832124a4d97a9b9e82380f7
Ihab Ali <47971028+h0o0bA@users.noreply.github.com>
2022-01-07 03:07:49 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import React, { Fragment, useEffect, useContext } from "react";
+import React, { Fragment } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Fragment>
+      <p>Test Running</p>
+    </Fragment>
+  );
+};
+
+export default App;
